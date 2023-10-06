765f0973bc91bf632f16913652e3cc224f3f4dbd
Junru Shao <junrushao1994@gmail.com>
2023-10-05 22:54:42 -0700

    Update latest MLC instructions

--- install/ssh.sh
+++
@@ -1,7 +0,0 @@
-set -euxo pipefail
-
-mkdir /var/run/sshd
-echo 'root:llm_perf' | chpasswd
-sed -ri 's/^#?PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -ri 's/^#?X11UseLocalhost\s+.*/X11UseLocalhost no/' /etc/ssh/sshd_config
-sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
